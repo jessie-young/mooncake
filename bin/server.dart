0dd5b7b324bbd31137d5f3aef08ca93646d6f0c1
Jessie Young <jessie@sahale.io>
2023-04-14 16:10:29 -0700

    lambda

--- bin/server.dart
+++ bin/server.dart
@@ -7,8 +7,13 @@ import 'package:shelf_router/shelf_router.dart';
 
 // Configure routes.
 final _router = Router()
+  ..get('/hello', _hello)
   ..get('/mooncake', _mooncakeHandler)
   ..get('/chookity', _chookityHandler);
+
+Response _hello(Request req) {
+  return Response.ok("hello");
+}
 
 Response _mooncakeHandler(Request req) {
   return Response.ok(mooncake);
@@ -18,15 +23,15 @@ Response _chookityHandler(Request req) {
   return Response.ok(chookity);
 }
 
-void main(List<String> args) async {
-  // Use any available host or container IP (usually `0.0.0.0`).
-  final ip = InternetAddress.anyIPv4;
+// void main(List<String> args) async {
+//   // Use any available host or container IP (usually `0.0.0.0`).
+final ip = InternetAddress.anyIPv4;
 
-  // Configure a pipeline that logs requests.
-  final handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
+// Configure a pipeline that logs requests.
+Handler handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
 
-  // For running in containers, we respect the PORT environment variable.
-  final port = int.parse(Platform.environment['PORT'] ?? '8080');
-  final server = await serve(handler, ip, port);
-  print('Server listening on port ${server.port}');
-}
+//   // For running in containers, we respect the PORT environment variable.
+//   final port = int.parse(Platform.environment['PORT'] ?? '8080');
+//   final server = await serve(handler, ip, port);
+//   print('Server listening on port ${server.port}');
+// }
