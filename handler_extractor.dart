e71e139cebcaa69905517bc2811836dd7de9a758
Jessie Young <jessie@sahale.io>
2023-04-17 23:59:34 -0700

    blah

--- handler_extractor.dart
+++ handler_extractor.dart
@@ -1,36 +1,59 @@
 import 'dart:io';
 
-void main(List<String> args) {
-  if (args.length != 2) {
-    print('Usage: dart add_handler.dart <filename> <handlername>');
+void main(List<String> arguments) {
+  if (arguments.isEmpty) {
+    print('Please provide a file path as an argument');
     exit(1);
   }
 
-  final file = args[0];
-  final tempFile = File('${file}.temp');
-  final handlerName = args[1];
-  final handlerDeclaration = 'Handler $handlerName;';
+  final filePath = arguments[0];
 
-  try {
-    // Create temporary file
-    tempFile.createSync();
-
-    // Copy contents of original file to temporary file, inserting handler declaration
-    final lines = File(file).readAsLinesSync();
-    final mainLine =
-        lines.indexWhere((line) => line.trim().startsWith('void main('));
-    final newLines = [
-      ...lines.sublist(0, mainLine),
-      handlerDeclaration,
-      ...lines.sublist(mainLine),
-    ];
-    tempFile.writeAsStringSync(newLines.join('\n'));
-
-    // Replace original file with temporary file
-    final originalFile = File(file);
-    originalFile.writeAsStringSync(tempFile.readAsStringSync());
-  } finally {
-    // Delete temporary file
-    tempFile.deleteSync();
+  final file = File(filePath);
+  if (!file.existsSync()) {
+    print('File does not exist: $filePath');
+    exit(1);
   }
+
+  final lines = file.readAsLinesSync();
+
+  int handlerLineIndex;
+  String handlerLine;
+
+  for (var i = 0; i < lines.length; i++) {
+    final line = lines[i];
+
+    if (line.trim().startsWith('Handler ')) {
+      handlerLineIndex = i;
+      handlerLine = line.trim();
+      print("found handler line");
+      print(handlerLine);
+      break;
+    }
+  }
+
+  if (handlerLineIndex == null) {
+    print('Handler definition not found in file: $filePath');
+    exit(1);
+  }
+
+  final modifiedLines = <String>[];
+
+  // Add all lines before the main function
+  for (var i = 0; i < lines.length; i++) {
+    final line = lines[i];
+
+    modifiedLines.add(line);
+    print("line: " + line);
+    if (line.trim().contains('void main')) {
+      // Duplicate handler definition line right before main function
+      modifiedLines.insert(i, handlerLine);
+      print("found main");
+    }
+  }
+
+  final modifiedContent = modifiedLines.join('\n');
+
+  file.writeAsStringSync(modifiedContent);
+
+  print('Handler definition added to file: $filePath');
 }
