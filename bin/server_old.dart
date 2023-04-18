84a2d951106638636add59851b4f6d6ee4793e30
Jessie Young <jessie@sahale.io>
2023-04-17 23:51:00 -0700

    blah

--- bin/server_old.dart
+++ bin/server_old.dart
@@ -28,13 +28,13 @@ Response _chookityHandler(Request req) {
   return Response.ok(chookity);
 }
 
-Handler handler;
+Handler handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
 void main(List<String> args) async {
   // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
 
   // Configure a pipeline that logs requests.
-  handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
+  // handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
 
   // For running in containers, we respect the PORT environment variable.
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
